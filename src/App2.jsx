b522d0ce75aaee30b0e22759f03a3a39ae22e076
mirzaatifbaig <63869168+mirzaatifbaig@users.noreply.github.com>
2025-01-07 21:05:57 +0500

    Add files via upload
    Simpler tic-tac-toe

---
+++ src/App2.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import PropTypes from "prop-types";
+
+function Square({ value, onSquareClick }) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+Square.propTypes = {
+    value: PropTypes.number.isRequired,
+    onSquareClick: PropTypes.func.isRequired,
+}// Assume you have a Square component
+
+export default function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+        if (squares[i]) return; // Prevent overwriting existing moves
+        const nextSquares = squares.slice();
+        nextSquares[i] = i % 2 === 0 ? "O" : "X";
+        setSquares(nextSquares);
+    }
+
+    return (
+        <>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)}  />
+            </div>
+        </>
+    )
+}
